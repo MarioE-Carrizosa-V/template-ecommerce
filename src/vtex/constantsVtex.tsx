f5bc9b4766b60fc1bb9cc9fd7d421175e9216f7d
Baruch Rodriguez <baruchbuc@gmail.com>
2023-10-18 16:36:27 -0600

    Se crea el contexto de sesión

---
+++ src/vtex/constantsVtex.tsx
@@ -0,0 +1,9 @@
+export const vtexAcountName = 'vinnerenio'
+export const vtexEnvironment = "vtexcommercestable"
+export const vtexEnvironmentIO = "myvtex"
+
+export const baseVTEX = 'https://'
+export const startLoginURL = '/api/vtexid/pub/authentication/startlogin'
+
+//NEXT_PUBLIC_VTEX_KEY=vtexappkey-vinneren-CGQXJO
+//NEXT_PUBLIC_VTEX_TOKEN=MJNNVKOWSCFDBXLVQIJURUBGEUPWYGQMRTDFNAUSXXZDMLPYGVIFMSSTPRIAHYEMJAMZBURGDCKZBBAXRTHYJJLWUXIJTARDPDXIVUHXLYLHUKTQTTWLTQYQAKGASNIO
