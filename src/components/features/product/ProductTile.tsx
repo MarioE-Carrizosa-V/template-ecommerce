c6d6aa6a6888d359cdcadbe5468dc9ec4e990c65
MarioE-Carrizosa-V <mytcarrizosaland@gmail.com>
2023-12-29 11:49:16 -0600

    Cambios en las cards de los productos

--- src/components/features/product/ProductTile.tsx
+++ src/components/features/product/ProductTile.tsx
@@ -1,10 +1,10 @@
 import { Box, Text } from '@chakra-ui/react';
 import { useContentfulInspectorMode } from '@contentful/live-preview/react';
 import Link from 'next/link';
-
 import { CtfImage } from '@src/components/features/contentful/ctf-image';
 import { FormatCurrency } from '@src/components/shared/format-currency';
 import { PageProductFieldsFragment } from '@src/lib/__generated/sdk';
+import { useState } from 'react';
 
 export const ProductTile = ({
   featuredProductImage,
@@ -14,13 +14,21 @@ export const ProductTile = ({
 }: PageProductFieldsFragment) => {
   const inspectorProps = useContentfulInspectorMode({ entryId });
 
+  const [hovered, setHovered] = useState(false);
+
+
   const shadow = {
     boxShadow: '0px 2px 6px 0px',
     transition: 'box-shadow 0.3s ease',
   };
 
+  const noShadow = {
+    boxShadow: 'none',
+  }
   return slug ? (
-    <div {...inspectorProps({ fieldId: 'featuredProductImage' })} style={shadow} onMouseOver={(e) => e.currentTarget.style.boxShadow = 'none'} onMouseOut={(e) => e.currentTarget.style.boxShadow = shadow.boxShadow}>
+    <div {...inspectorProps({ fieldId: 'featuredProductImage' })}       style={hovered? noShadow : shadow}
+    onMouseEnter={() => setHovered(true)}
+    onMouseLeave={() => setHovered(false)}>
       <Box as={Link} href={slug}>
         {featuredProductImage && (
           <Box borderRadius={4} overflow="hidden">
